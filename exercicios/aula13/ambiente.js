584faaeac28e8a3f8705a56390fb55a9dc7063af
Júlio Queiroz <jcqaraujo@gmail.com>
2024-07-18 22:33:40 -0400

    Repetições

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1 // teste lógico no final
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c<6)
+
+/* // teste lógico no inicio
+var c = 0
+while (c<6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
