f64415efacf23d72dd10c87c267052442c3e4815
Júlio Queiroz <jcqaraujo@gmail.com>
2024-07-22 18:46:43 -0400

    Update ambiente.js

--- exercicios/aula15/ambiente.js
+++ exercicios/aula15/ambiente.js
@@ -1,18 +1,15 @@
-var num = [5, 8, 2, 9, 3]
-
-console.log(`Nosso vetor é o ${num}`)
-
-num[5] = 6
-
-console.log(`Nosso vetor é o ${num}`)
-
-num[6] = 2
-num.push(1)
+var num = [5, 8, 2, 9, 3] // vetor
+console.log(`Nosso vetor é o ${num}`) // exibição do vetor
+num[5] = 6 // adicionar valores ao vetor na posição 5
+console.log(`Nosso vetor é o ${num}`) 
+num[6] = 2 // adicionar valor ao vetor na possição 6
+num.push(1) // adicionar valor ao vetor na ultima posição 
 num.sort()
 console.log(`Nosso vetor é o ${num}`)
 console.log(`O vetor tem ${num.length} posições`)
-console.log(num[3])
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+
 /*
 c = 0
 while (c<=num.length){
@@ -23,6 +20,8 @@ while (c<=num.length){
 /*for (var c = 0; c<num.length; c++){
     console.log(`A posição ${c} tem o valor ${num[c]}`)
 }*/
+
+// Exibir o vetor de forma mais elaborada com repetições
 
 for(var c in num){
     console.log(`A posição ${c} tem o valor ${num[c]}`)
