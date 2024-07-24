ddaf92daa8a652fb901a17fc07e35465dbb59bcb
Júlio Queiroz <jcqaraujo@gmail.com>
2024-07-24 19:06:02 -0400

    Update script.js

--- exercicios/exercicios01/pratica05/script.js
+++ exercicios/exercicios01/pratica05/script.js
@@ -2,6 +2,7 @@
 var num = []
 var list = document.getElementById('selnum')
 var res = document.getElementById('res')
+var maior = 0
 function adicionar() {
 var val = window.document.getElementById('txtn')
 if (val.value < 1 || val.value > 100){
@@ -16,6 +17,9 @@ if (val.value < 1 || val.value > 100){
             item.value = `list`
             list.appendChild(item)
             num.push(i)
+            if (maior < i){
+                maior = i
+            }
             res.innerHTML = ''
             val.value = ''
         }
@@ -31,7 +35,7 @@ function finalizar(){
         res.innerHTML = ''
         num.sort()
         res.innerHTML += `Ao todo, temos ${num.length} números cadastrados.`
-        res.innerHTML += `<br>O maior valor informado foi ${num[num.length]}`
+        res.innerHTML += `<br>O maior valor informado foi ${maior}`
         res.innerHTML += `<br>O menor número informado foi ${num[0]}`
         var soma = 0
         for (var c = 0; c < num.length; c++){ 
