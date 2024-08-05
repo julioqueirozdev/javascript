56da1a96545b08c9be6404bd07153762a579eb51
Júlio Queiroz <jcqaraujo@gmail.com>
2024-08-05 14:32:48 -0400

    desafios de js

---
+++ desafios/d001/script.js
@@ -0,0 +1,11 @@
+function bot1 (){
+    window.alert('Você clicou no 1º Botão!')
+}
+
+function bot2 (){
+    window.alert('Você clicou no 2º Botão!')
+}
+
+function bot3 (){
+    window.alert('Você clicou no 3º Botão!')
+}
---
+++ desafios/d003/desafio03.js
@@ -0,0 +1,6 @@
+function inicio (){
+    var num = window.prompt("Digite um número inteiro qualquer")
+    var i = Number(num)
+    window.alert(`Antes do númeoro ${i} temos o número ${i-1}.
+    Depois do número ${i} temos o número ${i+1}.`)
+}
