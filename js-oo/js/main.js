58c29ef9078fc9755d2f803118c0783b82ca9bbf
Júlio Queiroz <jcqaraujo@gmail.com>
2024-09-08 15:54:18 -0400

    js-oo

---
+++ js-oo/js/main.js
@@ -0,0 +1,29 @@
+
+class Pessoa 
+{
+    // atributos = variáveis ou caracteristicas
+    nome  // tipo string
+    idade  // tipo integer
+    peso // tipo float ou real 
+
+    // métodos = funções ou comportamentos 
+
+}
+
+// criar novo objeto a partir da classe, instanciar 
+
+let pessoa1 = new Pessoa()
+let pessoa2 = new Pessoa()
+
+// atribuir valores para os atributos de um objeto
+
+pessoa1.nome = 'Julio'
+pessoa1.idade = 29
+pessoa1.peso = 76
+
+pessoa2.nome = 'Ana'
+pessoa2.idade = 30
+pessoa2.peso = 65
+
+console.log(pessoa1)
+console.log(pessoa2)
