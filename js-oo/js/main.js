9ae0fde013c921c3371d86cf1a18bdd019aa68db
Júlio Queiroz <jcqaraujo@gmail.com>
2024-09-09 17:26:11 -0400

    att js oo

--- js-oo/js/main.js
+++ js-oo/js/main.js
@@ -1,29 +1,20 @@
 
-class Pessoa 
-{
-    // atributos = variáveis ou caracteristicas
-    _nome  // tipo string
-    _idade  // tipo integer
-    _peso // tipo float ou real
-    _altura //_ o underline indica que os atributos são privados
-
-    // métodos = funções ou comportamentos 
-    
-    // método construtor
-    constructor (nome, idade, peso, altura){
-        this._nome = nome
-        this._idade = idade
-        this._peso = peso
-        this._altura = altura
-
-    }
-
-}
+import {Pessoa} from './Pessoa.js'
 
 // criar novo objeto a partir da classe, instanciar 
 
 let pessoa1 = new Pessoa('Julio', 29, 76, 1.80)
 let pessoa2 = new Pessoa('Ana', 30, 65, 1.60)
+let pessoa3 = new Pessoa('Bia', 45, 60, 1.65)
+let pessoa4 = new Pessoa('Marco', 19, 70, 1.90)
+
+console.log(Pessoa.totalPessoas)
+console.log ('Situação ' + pessoa1.classificaIMC())
+
+console.log("Nome " + pessoa1.nome)
+console.log("Idade " + pessoa1.idade)
+console.log("IMC " + pessoa1.imc)
+
 
 // atribuir valores para os atributos de um objeto
 
@@ -39,3 +30,8 @@ let pessoa2 = new Pessoa('Ana', 30, 65, 1.60)
 
 console.log(pessoa1)
 console.log(pessoa2)
+console.log(pessoa3)
+console.log(pessoa4)
+
+
+console.log(pessoa1.calculaImc())
